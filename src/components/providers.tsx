0ca214a4ca32cd9a62f8b39f4efa0517cc93bbba
govindpvenu <govindpvenu.txt@gmail.com>
2025-10-09 10:37:57 +0530

    perf: remove jotai

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,39 +3,36 @@
 import { AppProgressProvider } from "@bprogress/next";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Provider as JotaiProvider } from "jotai";
 import dynamic from "next/dynamic";
 import { ThemeProvider } from "next-themes";
 
 const Toaster = dynamic(
   () => import("@/components/ui/sonner").then((mod) => mod.Toaster),
-  { ssr: false },
+  { ssr: false }
 );
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <JotaiProvider>
-      <ThemeProvider
-        enableSystem
-        disableTransitionOnChange
-        enableColorScheme
-        storageKey="theme"
-        defaultTheme="system"
-        attribute="class"
+    <ThemeProvider
+      enableSystem
+      disableTransitionOnChange
+      enableColorScheme
+      storageKey="theme"
+      defaultTheme="system"
+      attribute="class"
+    >
+      <AppProgressProvider
+        color="var(--foreground)"
+        height="2px"
+        delay={500}
+        options={{ showSpinner: false }}
       >
-        <AppProgressProvider
-          color="var(--foreground)"
-          height="2px"
-          delay={500}
-          options={{ showSpinner: false }}
-        >
-          {children}
-        </AppProgressProvider>
+        {children}
+      </AppProgressProvider>
 
-        <Toaster />
-        <Analytics />
-        <SpeedInsights />
-      </ThemeProvider>
-    </JotaiProvider>
+      <Toaster />
+      <Analytics />
+      <SpeedInsights />
+    </ThemeProvider>
   );
 }
